5cc16b87b31f377cf876b93d6094b1b047e9a450
eunsolyoon <todal160122@gmail.com>
2019-04-10 04:28:56 +0000

    로그인 로직  구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,8 @@ app_name = "accounts"
 urlpatterns = [
     #signup으로 들어오면 회원가입 폼 볼수있게 , 회원가입 시켜주기 작업이 필요함.
     path('signup/', views.signup, name='signup'),
+    #로그인 경로 만들어주기
+    path('login/', views.login, name='login'),
+    #로그아웃
+    path('logout/', views.logout, name='logout')
     ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth.forms import UserCreationForm
-
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout as auth_logout
 # Create your views here.
 def signup(request):
     if request.method == "POST":
@@ -13,5 +14,20 @@ def signup(request):
         form = UserCreationForm()
     #유저크리에이션 폼도 같이 넘기기
     #왜 리턴이 바깥에 있는가?
-    return render(request, 'accounts/signup.html',{'form':form})
-        
+    return render(request, 'accounts/form.html',{'form':form})
+    
+def login(request):
+    if request.method=="POST":
+        # 들어온 정보 인스턴스화
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            # 정보 잘 들어왔니?=>로그인 하기(세션에 저장)
+            auth_login(request, form.get_user())
+            return redirect("posts:list")
+    else:
+        form = AuthenticationForm() 
+    return render(request,'accounts/form.html',{"form":form})
+
+def logout(request):
+    auth_logout(request)
+    return redirect("posts:list")
